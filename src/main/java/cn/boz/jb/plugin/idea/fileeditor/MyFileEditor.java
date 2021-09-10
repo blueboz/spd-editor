a1afd81a7b4800e9dee84ff5f2fbcbfd076513d3
chenweidian-yfzx <chenweidian-yfzx@office.cgbchina>
2021-09-10 23:14:04 +0800

    fixed with some bugs!

---
+++ src/main/java/cn/boz/jb/plugin/idea/fileeditor/MyFileEditor.java
@@ -0,0 +1,88 @@
+package cn.boz.jb.plugin.idea.fileeditor;
+
+import cn.boz.jb.plugin.floweditor.gui.widget.ChartPanel;
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileEditorLocation;
+import com.intellij.openapi.fileEditor.FileEditorState;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.Nls;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.JComponent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+
+public class MyFileEditor implements FileEditor {
+
+    private VirtualFile virtualFile;
+
+    private ChartPanel chartPanel;
+
+    public MyFileEditor(VirtualFile virtualFile) {
+        this.virtualFile = virtualFile;
+        chartPanel = new ChartPanel();
+        chartPanel.loadFromFile(new File(virtualFile.getPath()));
+    }
+
+    @Override
+    public @NotNull JComponent getComponent() {
+        return chartPanel;
+    }
+
+    @Override
+    public @Nullable JComponent getPreferredFocusedComponent() {
+        return null;
+    }
+
+    @Override
+    public @Nls(capitalization = Nls.Capitalization.Title) @NotNull String getName() {
+        return "钿哥的流程编辑器";
+    }
+
+    @Override
+    public void setState(@NotNull FileEditorState fileEditorState) {
+
+    }
+
+    @Override
+    public boolean isModified() {
+        return false;
+    }
+
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+
+    @Override
+    public void addPropertyChangeListener(@NotNull PropertyChangeListener propertyChangeListener) {
+        System.out.println(propertyChangeListener);
+    }
+
+    @Override
+    public void removePropertyChangeListener(@NotNull PropertyChangeListener propertyChangeListener) {
+        System.out.println(propertyChangeListener);
+    }
+
+    @Override
+    public @Nullable FileEditorLocation getCurrentLocation() {
+        return null;
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public <T> @Nullable T getUserData(@NotNull Key<T> key) {
+        return null;
+    }
+
+    @Override
+    public <T> void putUserData(@NotNull Key<T> key, @Nullable T t) {
+
+    }
+}
---
+++ src/main/java/cn/boz/jb/plugin/idea/fileeditor/MyFileEditorProvider.java
@@ -0,0 +1,35 @@
+package cn.boz.jb.plugin.idea.fileeditor;
+
+import com.intellij.openapi.fileEditor.FileEditor;
+import com.intellij.openapi.fileEditor.FileEditorPolicy;
+import com.intellij.openapi.fileEditor.FileEditorProvider;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.NotNull;
+
+public class MyFileEditorProvider implements FileEditorProvider {
+    @Override
+    public boolean accept(@NotNull Project project, @NotNull VirtualFile virtualFile) {
+        String name = virtualFile.getName();
+        if (name.endsWith(".spd")) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public @NotNull FileEditor createEditor(@NotNull Project project, @NotNull VirtualFile virtualFile) {
+        return new MyFileEditor(virtualFile);
+    }
+
+    @Override
+    public @NotNull @NonNls String getEditorTypeId() {
+        return "myspdeditor";
+    }
+
+    @Override
+    public @NotNull FileEditorPolicy getPolicy() {
+        return FileEditorPolicy.PLACE_AFTER_DEFAULT_EDITOR;
+    }
+}
