8573b6657df4ffefbe11b7aa6ff61a9307dfc178
chenweidian-yfzx <chenweidian-yfzx@office.cgbchina>
2021-10-09 22:00:49 +0800

    with file template

--- src/main/java/cn/boz/jb/plugin/idea/action/NewSpdFileAction.java
+++ src/main/java/cn/boz/jb/plugin/idea/action/NewSpdFileAction.java
@@ -2,26 +2,21 @@ package cn.boz.jb.plugin.idea.action;
 
 import com.intellij.ide.actions.CreateFileFromTemplateAction;
 import com.intellij.ide.actions.CreateFileFromTemplateDialog;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.editor.Editor;
-import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.ide.fileTemplates.FileTemplateManager;
 import com.intellij.openapi.project.DumbAware;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.NlsActions;
 import com.intellij.openapi.util.NlsContexts;
 import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiElement;
-import com.intellij.util.Consumer;
-import com.intellij.util.SlowOperations;
+import icons.SpdEditorIcons;
 import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import javax.swing.Icon;
 
 public class NewSpdFileAction extends CreateFileFromTemplateAction implements DumbAware {
     public NewSpdFileAction() {
-        super("new Spd File","",null);
+        super("new Spd File", "", null);
     }
 
     public NewSpdFileAction(@NlsActions.ActionText String text, @NlsActions.ActionDescription String description, Icon icon) {
@@ -31,40 +26,14 @@ public class NewSpdFileAction extends CreateFileFromTemplateAction implements Du
     @Override
     protected void buildDialog(@NotNull Project project, @NotNull PsiDirectory psiDirectory, CreateFileFromTemplateDialog.@NotNull Builder builder) {
         builder.setTitle("Spd File")
-                .addKind("New Spd File",null,"spd");
-        builder.show("t1", "t2", new CreateFileFromTemplateDialog.FileCreator<PsiElement>() {
-            @Override
-            public @Nullable PsiElement createFile(@NonNls @NotNull String s, @NonNls @NotNull String s1) {
-                return null;
-            }
-
-            @Override
-            public @NlsContexts.Command @NotNull String getActionName(@NonNls @NotNull String s, @NonNls @NotNull String s1) {
-                return null;
-            }
-
-            @Override
-            public boolean startInWriteAction() {
-                return true;
-            }
-        }, new Consumer<PsiElement>() {
-            @Override
-            public void consume(PsiElement psiElement) {
-                if (psiElement != null) {
-                    Editor editor = FileEditorManager.getInstance(project).getSelectedTextEditor();
-                    System.out.println(psiElement);
-
-
-                }
-            }
-        });
+                .addKind("New Spd File", SpdEditorIcons.FLOW_16_ICON, "Spd Process.spd.ft");
+//        FileTemplateManager.getDefaultInstance().
     }
 
     @Override
     protected @NlsContexts.Command String getActionName(PsiDirectory psiDirectory, @NonNls @NotNull String s, @NonNls String s1) {
         return "new Spd file";
     }
-
 
 
 //    @Override
