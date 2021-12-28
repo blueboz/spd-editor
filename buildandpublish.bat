6a68d88a0599fca38fc093e2b9b52625d2328a93
chenweidian-yfzx <chenweidian-yfzx@office.cgbchina>
2021-12-28 14:58:33 +0800

    fixed some problems
    Change-Id: Ia2e33aab8d241f6efbcc571f9d0b3d86a747ca97

---
+++ buildandpublish.bat
@@ -0,0 +1,5 @@
+
+rem  编译插件
+gradle buildPlugin
+rem 上传插件
+gradle publishPluginLocal
--- src/main/java/cn/boz/jb/plugin/idea/toolwindow/PageRangeAction.java
+++ src/main/java/cn/boz/jb/plugin/idea/toolwindow/PageRangeAction.java
@@ -21,19 +21,20 @@ import java.util.List;
 public class PageRangeAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
-        InputEvent inputEvent = anActionEvent.getInputEvent();
-        Component component = inputEvent.getComponent();
-        List<DataSourceManager<?>> managers = DataSourceManager.getManagers(ProjectManager.getInstance().getDefaultProject());
-        DatabaseCredentials instance = DatabaseCredentials.getInstance();
-        DataSourceManagerDialog.showDialog(anActionEvent.getProject(), null, null);
-        for (DataSourceManager<?> manager : managers) {
-            List<DasDataSource> dataSources = (List<DasDataSource>) manager.getDataSources();
-            for (DasDataSource dataSource : dataSources) {
-                String name = dataSource.getName();
-                RawConnectionConfig connectionConfig = dataSource.getConnectionConfig();
-                System.out.println(connectionConfig);
-            }
-        }
+//        InputEvent inputEvent = anActionEvent.getInputEvent();
+//        Component component = inputEvent.getComponent();
+//        List<DataSourceManager<?>> managers = DataSourceManager.getManagers(ProjectManager.getInstance().getDefaultProject());
+//        DatabaseCredentials instance = DatabaseCredentials.getInstance();
+//
+//        DataSourceManagerDialog.showDialog(anActionEvent.getProject(), null, null);
+//        for (DataSourceManager<?> manager : managers) {
+//            List<DasDataSource> dataSources = (List<DasDataSource>) manager.getDataSources();
+//            for (DasDataSource dataSource : dataSources) {
+//                String name = dataSource.getName();
+//                RawConnectionConfig connectionConfig = dataSource.getConnectionConfig();
+//                System.out.println(connectionConfig);
+//            }
+//        }
     }
 
     @Override
