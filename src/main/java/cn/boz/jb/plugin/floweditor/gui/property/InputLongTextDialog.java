302626e8ae4afbb9686cfacc4fac4f4fbd5148cb
chenweidian-yfzx <chenweidian-yfzx@office.cgbchina>
2022-01-25 21:03:40 +0800

    fixed some bug's

--- src/main/java/cn/boz/jb/plugin/floweditor/gui/property/InputLongTextDialog.java
+++ src/main/java/cn/boz/jb/plugin/floweditor/gui/property/InputLongTextDialog.java
@@ -2,27 +2,35 @@ package cn.boz.jb.plugin.floweditor.gui.property;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.DialogWrapper;
+import com.intellij.ui.components.JBScrollPane;
 import org.jetbrains.annotations.Nullable;
 
 import javax.swing.JComponent;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import java.awt.Dimension;
 
 public class InputLongTextDialog extends DialogWrapper {
 
     private JTextArea textArea = null;
 
-    public InputLongTextDialog(@Nullable Project project, boolean canBeParent,String inputText) {
+    private JBScrollPane jScrollPane=null;
+
+    public InputLongTextDialog(@Nullable Project project, boolean canBeParent, String inputText) {
         super(project, canBeParent);
         this.textArea = new JTextArea();
         textArea.setText(inputText);
-        this.setSize(800, 600);
+        textArea.setAutoscrolls(true);
+//        textArea.setLineWrap(true);
+        jScrollPane = new JBScrollPane(textArea);
+        jScrollPane.setPreferredSize(new Dimension(800,600));
         init();
 //        this.setTitle("");
     }
 
     @Override
     protected @Nullable JComponent createCenterPanel() {
-        return this.textArea;
+        return this.jScrollPane;
     }
 
     public String getInputText() {
