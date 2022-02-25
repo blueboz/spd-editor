4b825f45b583cbbae8597b5b4ef7d11c3cfc2b45
chenweidian-yfzx <chenweidian-yfzx@office.cgbchina>
2022-02-25 18:18:36 +0800

    fixed so many bug's

---
+++ src/main/java/cn/boz/jb/plugin/idea/action/SpdCommitDiffAction.java
@@ -0,0 +1,14 @@
+package cn.boz.jb.plugin.idea.action;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class SpdCommitDiffAction extends AnAction {
+
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent anActionEvent) {
+        //比较选中版本
+    }
+
+}
